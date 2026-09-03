2d17bbf88237b7a787ce34af06f49c6ee0da5849
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-03 22:18:43 +0300

    Expose recently added anime endpoint

--- backend/anime/urls.py
+++ backend/anime/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
+
 from anime.api.views import (
     top_anime,
     anime_search,
     seasonal_anime,
     trending_anime,
-    anime_detail
+    recently_added_anime,
+    anime_detail,
 )
+
 
 urlpatterns = [
     path("top/", top_anime),
     path("search/", anime_search),
     path("seasonal/", seasonal_anime),
     path("trending/", trending_anime),
+    path("recently-added/", recently_added_anime),
     path("<int:anime_id>/", anime_detail),
-
 ]
