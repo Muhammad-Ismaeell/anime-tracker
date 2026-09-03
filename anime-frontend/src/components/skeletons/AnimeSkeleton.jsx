ba244b86ed2b54ed471c3a3a3c4da081e8cf206c
Muhammad-Ismaell <Muhammad-Ismaeell@users.noreply.github.com>
2026-09-03 17:13:50 +0300

    bleach null

--- anime-frontend/src/components/skeletons/AnimeSkeleton.jsx
+++ anime-frontend/src/components/skeletons/AnimeSkeleton.jsx
@@ -1,11 +1,11 @@
 export default function AnimeSkeleton() {
     return (
         <div className="anime-skeleton">
-            <div className="anime-skeleton-image shimmer" />
+            <div className="skeleton-image shimmer" />
 
-            <div className="anime-skeleton-content">
-                <div className="anime-skeleton-title shimmer" />
-                <div className="anime-skeleton-meta shimmer" />
+            <div className="skeleton-content">
+                <div className="skeleton-title shimmer" />
+                <div className="skeleton-meta shimmer" />
             </div>
         </div>
     );
