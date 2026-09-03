4d1757654d072b1b43d0820dbd5477dfa5e41ee7
Muhammad-Ismaell <Muhammad-Ismaeell@users.noreply.github.com>
2026-09-03 11:56:16 +0300

    bleach null

--- anime-frontend/src/components/GoogleSignIn.jsx
+++ anime-frontend/src/components/GoogleSignIn.jsx
@@ -1,6 +1,6 @@
 import { GoogleLogin } from "@react-oauth/google";
 
-export default function GoogleSignIn({onSuccess,onError}) {
+export default function GoogleSignIn({ onSuccess, onError }) {
 
     return (
         <GoogleLogin
