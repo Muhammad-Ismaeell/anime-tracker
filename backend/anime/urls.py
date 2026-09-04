37784ce54d79b89d6d35bb7b876703aa68748309
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-04 10:35:57 +0300

    Add anime statistics endpoint

--- backend/anime/urls.py
+++ backend/anime/urls.py
@@ -12,6 +12,7 @@ from anime.api.views import (
 from anime.api.episode_views import anime_episodes
 from anime.api.character_views import anime_characters
 from anime.api.staff_views import anime_staff
+from anime.api.statistics_views import anime_statistics
 
 
 urlpatterns = [
@@ -24,5 +25,6 @@ urlpatterns = [
     path("<int:anime_id>/episodes/", anime_episodes),
     path("<int:anime_id>/characters/", anime_characters),
     path("<int:anime_id>/staff/", anime_staff),
+    path("<int:anime_id>/statistics/", anime_statistics),
     path("<int:anime_id>/", anime_detail),
 ]
