746836a7556970dddf25fce29a740bb708842dfd
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-05 17:08:35 +0300

    Merge conflicting anime migrations

---
+++ backend/anime/migrations/0007_merge_0006_character_safety_and_index_rename.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("anime", "0006_charactersafety"),
+        ("anime", "0006_rename_anime_anime_created_9f1d2a_idx_anime_anime_created_1299d4_idx"),
+    ]
+
+    operations = []
