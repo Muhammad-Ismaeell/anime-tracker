f4b474dc898a7d5f2f7bf310217f02bf0ee6eba5
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-05 17:52:30 +0300

    Simplify general news API after removing search

--- backend/anime/infrastructure/jikan/jikan_client.py
+++ backend/anime/infrastructure/jikan/jikan_client.py
@@ -157,13 +157,8 @@ class JikanClient:
             params["letter"] = letter
         return self._get_list("characters", page, params)
 
-    def get_general_news(self, page=1, query="", tag="", limit=12):
-        params = {"limit": limit}
-        if query:
-            params["q"] = query
-        if tag:
-            params["tag"] = tag
-        return self._get_list("news", page, params)
+    def get_general_news(self, page=1):
+        return self._get_list("news", page, {"limit": 12})
 
     def get_detail(self, anime_id):
         data = safe_request(
