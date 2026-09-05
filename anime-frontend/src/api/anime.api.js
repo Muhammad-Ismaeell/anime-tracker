6857798b8c826d8e9c8d9fb653a7d19846a122e2
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-05 15:22:33 +0300

    Support discovery filters in anime API

--- anime-frontend/src/api/anime.api.js
+++ anime-frontend/src/api/anime.api.js
@@ -2,159 +2,84 @@ import api from "./client";
 
 export const AnimeAPI = {
     trending: async (page = 1) => {
-        const res = await api.get("/anime/trending/", {
-            params: { page },
-            skipAuth: true,
-        });
-
+        const res = await api.get("/anime/trending/", { params: { page }, skipAuth: true });
         return res.data;
     },
-
     seasonal: async (page = 1) => {
-        const res = await api.get("/anime/seasonal/", {
-            params: { page },
-            skipAuth: true,
-        });
-
+        const res = await api.get("/anime/seasonal/", { params: { page }, skipAuth: true });
         return res.data;
     },
-
     top: async (page = 1) => {
-        const res = await api.get("/anime/top/", {
-            params: { page },
-            skipAuth: true,
-        });
-
+        const res = await api.get("/anime/top/", { params: { page }, skipAuth: true });
         return res.data;
     },
-
     recentlyAdded: async (page = 1) => {
-        const res = await api.get("/anime/recently-added/", {
-            params: { page },
-            skipAuth: true,
-        });
-
+        const res = await api.get("/anime/recently-added/", { params: { page }, skipAuth: true });
         return res.data;
     },
-
     detail: async (id) => {
-        const res = await api.get(`/anime/${id}/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/`, { skipAuth: true });
         return res.data;
     },
-
     recommendations: async (id) => {
-        const res = await api.get(`/anime/${id}/recommendations/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/recommendations/`, { skipAuth: true });
         return res.data;
     },
-
     generalRecommendations: async (page = 1) => {
-        const res = await api.get("/anime/recommendations/", {
-            params: { page },
-            skipAuth: true,
-        });
-
+        const res = await api.get("/anime/recommendations/", { params: { page }, skipAuth: true });
         return res.data;
     },
-
     episodes: async (id, page = 1) => {
-        const res = await api.get(`/anime/${id}/episodes/`, {
-            params: { page },
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/episodes/`, { params: { page }, skipAuth: true });
         return res.data;
     },
-
     characters: async (id) => {
-        const res = await api.get(`/anime/${id}/characters/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/characters/`, { skipAuth: true });
         return res.data;
     },
-
-    generalCharacters: async (page = 1) => {
+    generalCharacters: async (page = 1, filters = {}) => {
         const res = await api.get("/anime/characters/", {
-            params: { page },
+            params: { page, ...filters },
             skipAuth: true,
         });
-
         return res.data;
     },
-
     staff: async (id) => {
-        const res = await api.get(`/anime/${id}/staff/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/staff/`, { skipAuth: true });
         return res.data;
     },
-
     statistics: async (id) => {
-        const res = await api.get(`/anime/${id}/statistics/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/statistics/`, { skipAuth: true });
         return res.data;
     },
-
     relations: async (id) => {
-        const res = await api.get(`/anime/${id}/relations/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/relations/`, { skipAuth: true });
         return res.data;
     },
-
     themes: async (id) => {
-        const res = await api.get(`/anime/${id}/themes/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/themes/`, { skipAuth: true });
         return res.data;
     },
-
     news: async (id) => {
-        const res = await api.get(`/anime/${id}/news/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/news/`, { skipAuth: true });
         return res.data;
     },
-
-    generalNews: async (page = 1) => {
+    generalNews: async (page = 1, filters = {}) => {
         const res = await api.get("/anime/news/", {
-            params: { page },
+            params: { page, ...filters },
             skipAuth: true,
         });
-
         return res.data;
     },
-
     externalLinks: async (id) => {
-        const res = await api.get(`/anime/${id}/external-links/`, {
-            skipAuth: true,
-        });
-
+        const res = await api.get(`/anime/${id}/external-links/`, { skipAuth: true });
         return res.data;
     },
-
     search: async ({ query, page = 1, filters = {} }) => {
         const res = await api.get("/anime/search/", {
-            params: {
-                q: query,
-                page,
-                ...filters,
-            },
+            params: { q: query, page, ...filters },
             skipAuth: true,
         });
-
         return res.data;
     },
 };
