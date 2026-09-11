b09b6b770490fd83a99ae6bdfb090c144613130a
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-11 11:58:56 +0300

    refactor(users): import anime serializers from presentation layer

--- backend/users/api/serializers/library_serializer.py
+++ backend/users/api/serializers/library_serializer.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 
+from anime.presentation.serializers import AnimeListSerializer
 from users.infrastructure.models import UserAnimeStatus
-from anime.api.serializers import AnimeListSerializer
 
 
 class LibrarySerializer(serializers.ModelSerializer):
-
     anime = AnimeListSerializer(read_only=True)
 
     class Meta:
