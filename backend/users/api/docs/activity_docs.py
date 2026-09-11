9540c7935d6a90130c17c62634d1a78b8f20acd3
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-11 11:59:12 +0300

    refactor(users): import anime serializers from presentation layer

--- backend/users/api/docs/activity_docs.py
+++ backend/users/api/docs/activity_docs.py
@@ -1,34 +1,17 @@
 from rest_framework import serializers
 
-from anime.api.serializers import AnimeSerializer
+from anime.presentation.serializers import AnimeSerializer
 
 
 class ActivityItemSerializer(serializers.Serializer):
-
     id = serializers.IntegerField()
-
     anime = AnimeSerializer()
-
     action = serializers.CharField()
-
     created_at = serializers.DateTimeField()
 
 
-
 class ActivityResponseSerializer(serializers.Serializer):
-
     count = serializers.IntegerField()
-
-    next = serializers.URLField(
-        allow_null=True
-    )
-
-    previous = serializers.URLField(
-        allow_null=True
-    )
-
-    results = ActivityItemSerializer(
-        many=True
-    )
-    
-    
+    next = serializers.URLField(allow_null=True)
+    previous = serializers.URLField(allow_null=True)
+    results = ActivityItemSerializer(many=True)
