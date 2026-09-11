d5b735b238e9b00affc6170da5d1d256ef051d79
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-11 11:51:24 +0300

    refactor(anime): align URL imports with feature views

--- backend/anime/urls.py
+++ backend/anime/urls.py
@@ -1,27 +1,22 @@
 from django.urls import path
 
 from anime.api.views import (
-    top_anime,
-    anime_search,
-    seasonal_anime,
-    trending_anime,
-    recently_added_anime,
-    anime_recommendations,
     anime_detail,
+    anime_search,
+    recently_added_anime,
+    seasonal_anime,
+    top_anime,
+    trending_anime,
 )
-from anime.api.discovery_views import (
-    general_recommendations,
-    general_characters,
-    general_news,
-)
+from anime.api.character_views import general_characters, anime_characters
 from anime.api.episode_views import anime_episodes
-from anime.api.character_views import anime_characters
+from anime.api.external_link_views import anime_external_links
+from anime.api.news_views import general_news, anime_news
+from anime.api.recommendation_views import general_recommendations, anime_recommendations
+from anime.api.relation_views import anime_relations
 from anime.api.staff_views import anime_staff
 from anime.api.statistics_views import anime_statistics
-from anime.api.relation_views import anime_relations
 from anime.api.theme_views import anime_themes
-from anime.api.news_views import anime_news
-from anime.api.external_link_views import anime_external_links
 
 
 urlpatterns = [
