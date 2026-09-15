bc01298c7cc5ebd8aa7694d4f7d424784c7fbe07
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:27:41 +0300

    Clean up anime episodes hook formatting

--- anime-frontend/src/hooks/useAnimeEpisodes.js
+++ anime-frontend/src/hooks/useAnimeEpisodes.js
@@ -5,13 +5,9 @@ import { AnimeAPI } from "../api/anime.api";
 export function useAnimeEpisodes(id) {
     return useInfiniteQuery({
         queryKey: ["anime-episodes", id],
-
-        queryFn: ({ pageParam = 1 }) =>
-            AnimeAPI.episodes(id, pageParam),
-
+        queryFn: ({ pageParam = 1 }) => AnimeAPI.episodes(id, pageParam),
         enabled: !!id,
         initialPageParam: 1,
-
         getNextPageParam: (lastPage) => {
             if (!lastPage?.has_next) {
                 return undefined;
@@ -19,7 +15,6 @@ export function useAnimeEpisodes(id) {
 
             return (lastPage.page ?? 1) + 1;
         },
-
         staleTime: 1000 * 60 * 30,
     });
 }
