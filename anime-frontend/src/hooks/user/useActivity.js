369ef4f91e881c0fc6a52d6b7991cb397860466d
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:36:03 +0300

    Clean up activity hook formatting

--- anime-frontend/src/hooks/user/useActivity.js
+++ anime-frontend/src/hooks/user/useActivity.js
@@ -1,31 +1,23 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+
 import { UserAPI } from "../../api/user.api";
 import { queryKeys } from "../../lib/querykeys";
 
 export function useActivity() {
     return useInfiniteQuery({
-
         queryKey: queryKeys.users.activity,
-
         queryFn: async ({ pageParam = 1 }) => {
             const res = await UserAPI.activity(pageParam);
             return res.data;
         },
-
         initialPageParam: 1,
-
         getNextPageParam: (lastPage) => {
-
             if (!lastPage?.next) {
                 return undefined;
             }
 
             const url = new URL(lastPage.next);
-
-            return Number(
-                url.searchParams.get("page")
-            );
+            return Number(url.searchParams.get("page"));
         },
-
     });
 }
