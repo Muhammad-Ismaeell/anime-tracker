933de49f553b0e5b667b04fe4f632a20679b66db
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:10:44 +0300

    Clean up profile serializer formatting

--- backend/users/api/serializers/profile_serializer.py
+++ backend/users/api/serializers/profile_serializer.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 
 from users.infrastructure.models import Profile
 
-class ProfileSerializer(serializers.ModelSerializer):
 
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
