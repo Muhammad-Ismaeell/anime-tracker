2a134b107015ebfc0e42629171e0d9e585d858bc
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 09:31:15 +0300

    Clean up anime URL formatting

--- backend/anime/urls.py
+++ backend/anime/urls.py
@@ -8,16 +8,18 @@ from anime.api.anime_views import (
     top_anime,
     trending_anime,
 )
-from anime.api.character_views import general_characters, anime_characters
+from anime.api.character_views import anime_characters, general_characters
 from anime.api.episode_views import anime_episodes
 from anime.api.external_link_views import anime_external_links
-from anime.api.news_views import general_news, anime_news
-from anime.api.recommendation_views import general_recommendations, anime_recommendations
+from anime.api.news_views import anime_news, general_news
+from anime.api.recommendation_views import (
+    anime_recommendations,
+    general_recommendations,
+)
 from anime.api.relation_views import anime_relations
 from anime.api.staff_views import anime_staff
 from anime.api.statistics_views import anime_statistics
 from anime.api.theme_views import anime_themes
-
 
 urlpatterns = [
     path("top/", top_anime),
