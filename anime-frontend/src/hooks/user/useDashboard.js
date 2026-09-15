986d47cdce96eb361f7c786ba1815769791c2497
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:36:12 +0300

    Clean up dashboard hook formatting

--- anime-frontend/src/hooks/user/useDashboard.js
+++ anime-frontend/src/hooks/user/useDashboard.js
@@ -1,26 +1,14 @@
-
 import { useQuery } from "@tanstack/react-query";
+
 import api from "../../api/client";
 import { queryKeys } from "../../lib/querykeys";
 
-
 export function useDashboard() {
-
     return useQuery({
-
         queryKey: queryKeys.users.dashboard,
-
         queryFn: async () => {
-
-            const response = await api.get(
-                "/users/dashboard/"
-            );
-
+            const response = await api.get("/users/dashboard/");
             return response.data?.data ?? {};
-
         },
-
     });
-
 }
-
