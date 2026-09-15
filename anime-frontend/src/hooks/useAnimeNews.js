d5b232112e91c45abd6d3fd28b728e1894dac088
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:25:48 +0300

    Clean up anime news hook formatting

--- anime-frontend/src/hooks/useAnimeNews.js
+++ anime-frontend/src/hooks/useAnimeNews.js
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 
 import { AnimeAPI } from "../api/anime.api";
 
-
 export function useAnimeNews(animeId) {
     return useQuery({
         queryKey: ["anime-news", animeId],
