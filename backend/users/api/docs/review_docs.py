cc6142270315a2e5e4a435d7dc308ee6257bbd2c
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 09:28:43 +0300

    Clean up review API serializers

--- backend/users/api/docs/review_docs.py
+++ backend/users/api/docs/review_docs.py
@@ -2,30 +2,15 @@ from rest_framework import serializers
 
 
 class ReviewCreateRequestSerializer(serializers.Serializer):
-
     anime_id = serializers.IntegerField()
-
-    rating = serializers.IntegerField(
-        min_value=1,
-        max_value=10
-    )
-
-    text = serializers.CharField(
-        required=False,
-        allow_blank=True
-    )
-
+    rating = serializers.IntegerField(min_value=1, max_value=10)
+    text = serializers.CharField(required=False, allow_blank=True)
 
 
 class ReviewUpdateRequestSerializer(serializers.Serializer):
-
     rating = serializers.IntegerField(
         required=False,
         min_value=1,
-        max_value=10
+        max_value=10,
     )
-
-    text = serializers.CharField(
-        required=False,
-        allow_blank=True
-    )
+    text = serializers.CharField(required=False, allow_blank=True)
