4b768fe74e1bd55c48a8e91e33f2e591dadc893f
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:25:21 +0300

    Clean up anime characters hook formatting

--- anime-frontend/src/hooks/useAnimeCharacters.js
+++ anime-frontend/src/hooks/useAnimeCharacters.js
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 
 import { AnimeAPI } from "../api/anime.api";
 
-
 export function useAnimeCharacters(id) {
     return useQuery({
         queryKey: ["anime-characters", id],
