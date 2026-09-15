1e5bb989b75ffe57e0e21b94bb37361473b931b0
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:25:13 +0300

    Clean up anime detail hook formatting

--- anime-frontend/src/hooks/useAnimeDetail.js
+++ anime-frontend/src/hooks/useAnimeDetail.js
@@ -4,28 +4,13 @@ import { AnimeAPI } from "../api/anime.api";
 import { normalizeAnime } from "../utils/normalizeAnime";
 
 export function useAnimeDetail(id) {
-
     return useQuery({
-
-        queryKey: [
-            "anime-detail",
-            id
-        ],
-
+        queryKey: ["anime-detail", id],
         queryFn: async () => {
-
             const data = await AnimeAPI.detail(id);
-
-            return normalizeAnime(
-                data.item
-            );
+            return normalizeAnime(data.item);
         },
-
         enabled: !!id,
-
-        // Do not show the previous anime while navigating
-        // from one detail page to another.
         placeholderData: undefined,
-
     });
 }
