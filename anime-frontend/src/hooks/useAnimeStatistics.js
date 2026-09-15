5bb8c467d02aadb5fe099c675577545f4cb626f5
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:26:15 +0300

    Clean up anime statistics hook formatting

--- anime-frontend/src/hooks/useAnimeStatistics.js
+++ anime-frontend/src/hooks/useAnimeStatistics.js
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 
 import { AnimeAPI } from "../api/anime.api";
 
-
 export function useAnimeStatistics(animeId) {
     return useQuery({
         queryKey: ["anime-statistics", animeId],
