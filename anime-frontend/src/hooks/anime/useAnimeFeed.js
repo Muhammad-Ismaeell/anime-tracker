8abb7222b7f104cafb8cd6abf03721a0f7ecf028
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 13:25:02 +0300

    Clean up anime feed hook formatting

--- anime-frontend/src/hooks/anime/useAnimeFeed.js
+++ anime-frontend/src/hooks/anime/useAnimeFeed.js
@@ -1,21 +1,13 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+
 import { AnimeAPI } from "../../api/anime.api";
 
-
 export function useAnimeFeed(type) {
-
     return useInfiniteQuery({
-
         queryKey: ["anime-feed", type],
-
-        queryFn: ({ pageParam = 1 }) =>
-            AnimeAPI[type](pageParam),
-
+        queryFn: ({ pageParam = 1 }) => AnimeAPI[type](pageParam),
         initialPageParam: 1,
-
         getNextPageParam: (lastPage) =>
-            lastPage.has_next
-                ? lastPage.page + 1
-                : undefined
+            lastPage.has_next ? lastPage.page + 1 : undefined,
     });
 }
