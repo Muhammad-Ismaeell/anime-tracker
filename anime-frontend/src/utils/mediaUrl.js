946390a121d423bce4e2363e3053a39508d1b2a6
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 09:20:29 +0300

    Clean up media URL helper

--- anime-frontend/src/utils/mediaUrl.js
+++ anime-frontend/src/utils/mediaUrl.js
@@ -1,15 +1,11 @@
 export function getMediaUrl(path) {
-
     if (!path) {
         return "/no-image.png";
     }
-
 
     if (path.startsWith("http")) {
         return path;
     }
 
-
     return `${import.meta.env.VITE_MEDIA_URL}${path}`;
-
 }
