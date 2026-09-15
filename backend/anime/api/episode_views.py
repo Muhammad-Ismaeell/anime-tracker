b1f14e793bcf7bf6c72d344af1718a5fbee163df
Muhammad-Ismaeell <147401066+Muhammad-Ismaeell@users.noreply.github.com>
2026-09-15 09:20:21 +0300

    Clean up episode API view

--- backend/anime/api/episode_views.py
+++ backend/anime/api/episode_views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, OpenApiTypes, extend_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -24,11 +24,10 @@ episode_service = EpisodeService()
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def anime_episodes(request, anime_id):
+    """Return one page of episode information for an anime."""
     try:
         page = max(1, int(request.GET.get("page", 1)))
     except (TypeError, ValueError):
         page = 1
 
-    return Response(
-        episode_service.get_episodes(anime_id, page)
-    )
+    return Response(episode_service.get_episodes(anime_id, page))
